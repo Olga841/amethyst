2b42cd6b899241c7aef90fcdeb07fb815ab2fc08
Olga Alekseeva <olgaalekseeva966@gmail.com>
2025-04-21 13:29:12 +0300

    add: icons of likes, comments & share

--- src/components/Posts/PostItem/PostItem.jsx
+++ src/components/Posts/PostItem/PostItem.jsx
@@ -8,12 +8,14 @@ const PostItem = (props) => {
   return (
     <div className={s.PostItem}>
       <div className={s.PostItem_wrapper}>
-        <img src={props.avatar} alt="avatar" className={s.PostItem_img} />
-        <div className={s.PostItem_text}>{props.message}</div>
-        <div className="PostItem_icons">
-          <img src={like} alt="like" className="PostItem_icon" />
-          <img src={comment} alt="comments" className="PostItem_icon" />
-          <img src={share} alt="share" className="PostItem_icon" />
+        <div className={s.PostItem_container}>
+          <img src={props.avatar} alt="avatar" className={s.PostItem_img} />
+          <div className={s.PostItem_text}>{props.message}</div>
+        </div>
+        <div className={s.PostItem_icons}>
+          <img src={like} alt="like" className={s.PostItem_icon} />
+          <img src={comment} alt="comments" className={s.PostItem_icon} />
+          <img src={share} alt="share" className={s.PostItem_icon} />
         </div>
       </div>
     </div>
