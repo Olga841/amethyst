2a907caa41f1e0605a3ac6820520d1ddbba3218d
Olga Alekseeva <olgaalekseeva966@gmail.com>
2025-04-27 19:03:31 +0300

    add: data to state

--- src/index.js
+++ src/index.js
@@ -4,30 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-let posts = [
-  {
-    postId: 1,
-    message: "hfhdzghjp;lkyutrdsfghujio[/.kjhgfdxfghjkl",
-  },
-  {
-    postId: 2,
-    message: "456",
-  },
-  {
-    postId: 3,
-    message: "789",
-  },
-];
 
-let dialogs = [
-  {id: 1, userData: "Olga", message: "Lorem ipsum", date: "today", time: "11:24"},
-  {id: 2, userData: "Sveta", message: "Lorem ipsum", date: "yesterday", time: "11:24"},
-  {id: 3, userData: "Olesya", message: "Lorem ipsum", date: "yesterday", time: "14:18"},
-  {id: 4, userData: "Ksyusha", message: "Lorem ipsum", date: "2 days ago", time: null },
-  {id: 5, userData: "Liza", message: "Lorem ipsum", date: "2 days ago", time: null},
-  {id: 6, userData: "Natasha", message: "Lorem ipsum", date: "last week", time: null},
-  {id: 7, userData: "Andrey", message: "Lorem ipsum", date: "last week", time: null},
-];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/state.js
@@ -0,0 +1,28 @@
+let state = {
+    profilePage: {
+        posts: [{
+            postId: 1,
+            message: "hfhdzghjp;lkyutrdsfghujio[/.kjhgfdxfghjkl",
+        },
+        {
+            postId: 2,
+            message: "456",
+        },
+        {
+            postId: 3,
+            message: "789",
+        }
+        ]
+    },
+    dialogsPage: {
+        dialogs: [
+            { id: 1, userData: "Olga", message: "Lorem ipsum", date: "today", time: "11:24" },
+            { id: 2, userData: "Sveta", message: "Lorem ipsum", date: "yesterday", time: "11:24" },
+            { id: 3, userData: "Olesya", message: "Lorem ipsum", date: "yesterday", time: "14:18" },
+            { id: 4, userData: "Ksyusha", message: "Lorem ipsum", date: "2 days ago", time: null },
+            { id: 5, userData: "Liza", message: "Lorem ipsum", date: "2 days ago", time: null },
+            { id: 6, userData: "Natasha", message: "Lorem ipsum", date: "last week", time: null },
+            { id: 7, userData: "Andrey", message: "Lorem ipsum", date: "last week", time: null },
+        ]
+    }
+}
